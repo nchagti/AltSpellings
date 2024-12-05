e5f69033e330c570f7165d39e3d3984d0d60b305
Nitya Chagti <99576680+nchagti@users.noreply.github.com>
2024-12-04 20:07:32 -0500

    Standardize and update alternate spellings
    Python script that takes a list of words to standardize their descriptions and format, and lists all alternate spellings in the definition

---
+++ alt_spellings.py
@@ -0,0 +1,30 @@
+import csv
+
+file_path = './test_words.txt' #enter the file path for your text file with all words 
+with open(file_path, 'r', encoding='utf-8') as f:
+    words = [line.strip() for line in f.readlines()]
+singular_words = [word for word in words if not word.endswith("S")]
+plural_words = [word for word in words if word.endswith("S")]
+single_word = ''
+description = '(Arabic) a cloak with a hood and wide sleeves' #the standardized description of the word that will apply to all alternate spellings
+
+csv_data = []
+for word in words:
+    # Alternate spellings: exclude the word itself and plurals if the word is singular
+    if word.endswith("S"):  # Plural word
+        single_word = word[:-1] 
+        alternate_spellings = sorted([spelling for spelling in singular_words if spelling != single_word])
+        suffix = "[n]"
+        definition = f"{single_word.upper()}, {description}, also {', '.join(alternate_spellings)} {suffix}"
+    else:  # Singular word
+        alternate_spellings = sorted([spelling for spelling in singular_words if spelling != word])
+        suffix = "[n -S]"
+    # Construct the definition
+        definition = f"{description}, also {', '.join(alternate_spellings)} {suffix}"
+    csv_data.append([word, definition])
+    
+output_file = './test_definitions'
+with open(output_file, "w", newline="", encoding="utf-8") as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(["Word", "Definition"])
+    csv_writer.writerows(csv_data)
